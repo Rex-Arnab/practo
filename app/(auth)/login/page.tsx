9b49c7e2c7823d71e554ad81ef0205e9388cba87
Arnab Biswas <khanp4397@gmail.com>
2023-10-29 19:42:15 +0530

    added password eye

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast";
-import { Loader } from "lucide-react";
+import { Eye, EyeOff, Loader } from "lucide-react";
 import { useState } from "react";
 
 function LoginPage() {
@@ -12,6 +12,7 @@ function LoginPage() {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const { toast } = useToast();
+  const [togglePassword, setTogglePassword] = useState(false);
 
   const handleLogin = async () => {
     setLoading(true);
@@ -31,16 +32,31 @@ function LoginPage() {
         <h1 className="text-2xl font-bold mt-10">Login To Your Account</h1>
         <div className="p-5 bg-white rounded shadow space-y-5">
           <Input
-            placeholder="Enter Your Email"
+            placeholder="Enter Your email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
 
-          <Input
-            placeholder="Enter Your Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <div className="flex items-center justify-between mr-2">
+            <Input
+              placeholder="Enter Your password"
+              type={togglePassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="mr-2"
+            />
+            {togglePassword ? (
+              <Eye
+                className="h-5 w-5 cursor-pointer select-none"
+                onClick={() => setTogglePassword(false)}
+              />
+            ) : (
+              <EyeOff
+                className="h-5 w-5 cursor-pointer select-none"
+                onClick={() => setTogglePassword(true)}
+              />
+            )}
+          </div>
 
           <Button className="w-full" onClick={handleLogin}>
             {loading ? <Loader className="h-4 w-4 animate-spin" /> : "Login"}
